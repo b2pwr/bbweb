1ceb6c0211c75fb7217e68d304b6eee11afdd4b3
b2pwr <e54096409@gmail.com>
2017-08-09 15:19:18 +0800

    modify default content

--- src/Default.js
+++ src/Default.js
@@ -8,7 +8,9 @@ class Default extends React.Component {
         <h1 className="intro">INTRO</h1>
         <h2 className="subIntro">簡介</h2>
         <p className="introContent">
-          這是一個利用antd和react寫成的個人網頁，如果有任何問題請聯繫我。
+          剛從陰間歸來，有一種在叢林生活久了，還無法適應城市的感覺。在巴黎、冰島以及澳洲旅遊了一陣子。<br />
+          喜歡插畫塗鴉、攝影還有Js，會AI和一點PS，常常在用LR修圖。 <br />
+          不斷在學習前端技術，希望成為一個好的前端工程師。
         </p>
       </div> 
     )
